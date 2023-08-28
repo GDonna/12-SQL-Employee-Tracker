8af3a8c57d0644b2f29b4a9f41f8a25171d3c7d3
Donna Goon <g.donna120@gmail.com>
2023-08-28 18:12:12 -0500

    added inquire to server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const inquirer = require('inquirer');
 // Import and require mysql2
 const mysql = require('mysql2');
 
@@ -21,6 +22,55 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the books_db database.`)
 );
+
+function startApp() {
+  // Use inquirer to prompt user for options
+  inquirer
+    .prompt({
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ]
+    })
+    .then(answer => {
+      switch (answer.action) {
+        case 'View all departments':
+          // Call function to view departments
+          break;
+        case 'View all roles':
+          // Call function to view roles
+          break;
+        case 'View all employees':
+          // Call function to view employees
+          break;
+        case 'Add a department':
+          // Call function to add department
+          break;
+        case 'Add a role':
+          // Call function to add role
+          break;
+        case 'Add an employee':
+          // Call function to add employee
+          break;
+        case 'Update an employee role':
+          // Call function to update employee role
+          break;
+        case 'Exit':
+          // Exit the application
+          console.log('Goodbye!');
+          process.exit(0);
+      }
+    });
+}
 
 // Query database using COUNT() and GROUP BY
 db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
