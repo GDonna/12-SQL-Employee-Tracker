855adfdf8b44ae8156986a9b0fc6e55efd05933c
Donna Goon <g.donna120@gmail.com>
2023-08-27 21:43:46 -0500

    added role seeds

---
+++ db/role_seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO role_table (id, title, salary)
+VALUES (001, "Sales Account Manager", "100,000.00"),
+       (002, "Vice President", "300,000.00"),
+       (003, "Account Operation Manager", "65,000.00"),
+       (004, "Customer Service", "55,000.00"),
+       (005, "Receptionist", "45,000.00");
+       
--- db/schema.sql
+++ db/schema.sql
@@ -4,26 +4,30 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE department (
-  id INT NOT NULL,
-  name VARCHAR(30) NOT NULL
-);
+CREATE TABLE
+    department (
+        id INT NOT NULL,
+        name VARCHAR(30) NOT NULL,
+        PRIMARY KEY (id)
+    );
 
-CREATE TABLE role (
-    id INT NOT NULL,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT
-)
+CREATE TABLE
+    role_table (
+        id INT NOT NULL,
+        title VARCHAR(30),
+        salary DECIMAL(20,2),
+        department_id INT,
+        FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE
+        SET NULL
+    )
 
-CREATE TABLE employee (
-    id INT NOT NULL,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT
-   
-    department_id INT
-)
-
-
+CREATE TABLE
+    employee (
+        id INT NOT NULL,
+        first_name VARCHAR(30),
+        last_name VARCHAR(30),
+        role_id INT,
+        manager_id INT,
+        FOREIGN KEY (role_id) REFERENCES role_table(id) ON DELETE
+        SET NULL
+    )
