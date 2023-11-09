0c06a03d812f2ef672c224fcd51dc74258ed0f42
Donna Goon <g.donna120@gmail.com>
2023-11-09 15:50:37 -0600

    edit package json

--- db/role_seeds.sql
+++ db/role_seeds.sql
@@ -1,18 +1,17 @@
 USE employee_db;
+       
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Administration");
 INSERT INTO role_table (title, salary, department_id)
 VALUES ("Sales Account Manager", "100000", 1),
        ( "Vice President", "300000", 2),
        ("Account Operation Manager", "65000", 1),
        ("Customer Service", "55000", 1),
        ("Receptionist", "45000", 2);
-       
-INSERT INTO department (name)
-VALUES ("Sales"),
-       ("Administration");
-
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Jenny", "Smith", 4, NULL),
         ("Adam", "Tucker", 1, 1),
         ("Sally", "Jones", 2, NULL),
         ("John", "Doe", 3, 3),
-        ("Jane", "Doe", 5, 4);
+        ("Jane", "Doe", 5, 1);
